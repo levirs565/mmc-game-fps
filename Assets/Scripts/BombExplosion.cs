3dd6d4b03cb75386142a2787cc52b705d29c6748
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-10-14 20:11:13 +0700

    rename bomb script

--- Assets/Scripts/BombExplosion.cs
+++ Assets/Scripts/BombExplosion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour
+public class BombExplosion : MonoBehaviour
 {
     private ZombieManager mZombieManager;
 
